8ee96817d9315293d2246472e9f1ac34f146572a
zhangruofan <nodeman@outlook.com>
2016-05-19 20:24:20 +0800

    add organize

--- src/models/admin.js
+++ src/models/admin.js
@@ -1,43 +1,110 @@
 import md5 from 'md5'
 import config from '../constants/config.js'
-import {isEmpty} from '../utils/validation.js'
-import Code from '../constants/Code.js'
+import {
+	isEmpty
+} from '../utils/validation.js'
+import {
+	CODE,
+	EMPTY,
+	RESULT
+} from '../constants/enumerate.js'
 import fetch from 'isomorphic-fetch'
 
-const INITMSG = 'unkown error'
-const EMPTY = 'arguments is empty'
 const admin = {
-	login: async (account,pwd)=>{
-		let result = {
-			code : Code.INIT,
-			msg : INITMSG
-		}
-		if(isEmpty(account) || isEmpty(pwd)){
+	login: (account, pwd) => {
+		let result = Object.assign({},RESULT,{data:{}})
+		if (isEmpty(account) || isEmpty(pwd)) {
 			result.msg = EMPTY
 			return result
 		}
-		await fetch(`${config.api}admin/login?account=${account}&pwd=${md5(pwd)}`)
-		.then(response=>{
+		return fetch(`${config.api}admin/login?account=${account}&pwd=${md5(pwd)}`, {
+			method: 'POST'
+		}).then(response => {
+			if (response.ok) {
+				return response.json()
+			} else {
+				result.msg = response.statusText
+				return Promise.reject(result.msg)
+			}
+		}).then(data => {
+			if (data.code === CODE.NORMAL) {
+				result.code = CODE.NORMAL
+				result.data.key = data.key
+				result.data.token = data.token
+			} else {
+				result.msg = data.msg
+			}
+			return result
+		})
+	},
+	changePwd: (key, token, pwd) => {
+		let result = {
+			code: CODE.INIT,
+			msg: INITMSG
+		}
+		if (isEmpty(key) || isEmpty(token) || isEmpty(pwd)) {
+			result.msg = EMPTY
+			return result
+		}
+		return fetch(`${config.api}admin/put?key=${key}&token=${token}&pwd=${md5(pwd)}`, {
+			method: 'PUT'
+		}).then(response => {
 			if(response.ok){
 				return response.json()
 			}else{
 				result.msg = response.statusText
-				return Promise.reject(result.msg)
+				return Promise.reject(result)
 			}
-		}).then(data=>{
-			console.log(data)
-			data.code === Code.NORMAL ? result.code = Code.NORMAL:result.msg = data.msg
+		}).then(data => {
+			data.code === CODE.NORMAL ? result.code = CODE.NORMAL : result.msg = data.msg
+			return result
 		})
-		return result
 	},
-	changePwd:async (key,token,pwd)=>{
+	logout: (key, token) => {
 		let result = {
-			code : Code.INIT,
-			msg : INITMSG
+			code: CODE.INIT,
+			msg: INITMSG
 		}
-		if(isEmpty(key) || isEmpty(token) || isEmpty(pwd)){
-
+		if (isEmpty(key) || isEmpty(token)) {
+			result.msg = EMPTY
+			return result
 		}
+		return fetch(`${config.api}admin/logout?key=${key}&token=${token}`, {
+			method: 'get'
+		}).then(response => {
+			if (response.ok) {
+				return response.json()
+			} else {
+				result.msg = response.statusText
+				return Promise.reject(result)
+			}
+		}).then(data => {
+			data.code === CODE.NORMAL ? result.code = CODE.NORMAL : result.msg = data.msg
+			return result
+		})
+	},
+	valid: (key, token) => {
+		let result = {
+			code: CODE.INIT,
+			msg: INITMSG
+		}
+		if (isEmpty(key) || isEmpty(token)) {
+			result.msg = EMPTY
+			return result
+		}
+		return fetch(`${config.api}admin/valid?key=${key}&token=${token}`, {
+			method: 'get'
+		}).then(response => {
+			if (response.ok) {
+				return response.json()
+			} else {
+				result.msg = response.statusText
+				return Promise.reject(result)
+			}
+		}).then(data => {
+			data.code === CODE.NORMAL ? result.code = CODE.NORMAL : result.msg = data.msg
+			return result
+		})
 	}
 }
 
--- test/models/admin.spec.js
+++ test/models/admin.spec.js
@@ -1,8 +1,33 @@
 import test from 'ava'
 import admin from '../../src/models/admin'
-import md5 from 'md5'
+import {
+	CODE
+} from '../../src/constants/enumerate.js'
 
-test('admin login',async t => {
+const key = 2
+var token
+test('admin login failed',async t => {
+	const result = await admin.login('zhangruofan','12345')
+	const expectResult = {
+		code:CODE.INIT,
+		msg:'密码错误'
+	}
+	t.deepEqual(result,expectResult)
+})
+
+test('admin login successfully',async t => {
 	const result = await admin.login('zhangruofan','123456')
-	t.is(result.code, 0)
+	t.is(result.code,CODE.NORMAL)
+	t.is(result.data.key,key)
+	t.true(result.data.token.length > 0)
+	token = result.data.token
+})
+
+test.after('after admin login',async t => {
+	const valid = await admin.valid(key,token)
+	t.is(valid.code,CODE.NORMAL,'key token is valid')
+	const changePwd = await admin.changePwd(key,token,'123456')
+	t.is(changePwd.code,CODE.NORMAL,'change password to 123456')
+	const logout = await admin.logout(key,token)
+	t.is(logout.code,CODE.NORMAL,'login out successfully')
 })
